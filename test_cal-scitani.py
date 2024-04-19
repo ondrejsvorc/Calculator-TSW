4bdcbaee90946130a8cb913530b9697fa67fa35d
Ondřej Švorc <ondrejsvorc@email.cz>
2024-04-19 18:39:45 +0200

    tttt

---
+++ test_cal-scitani.py
@@ -0,0 +1,17 @@
+import pytest
+from cal_scitani import Calculator
+
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+
+@pytest.mark.parametrize("a,b,out", [(1, 2, 3), (-1, -2, -3), (5, -7, -2)])
+def test_plus(a, b, out, calculator):
+    assert calculator.plus(a, b) == out
+
+
+@pytest.mark.parametrize("a,b,out", [(1, 2, -1), (3, 2, 1), (5, 7, -2)])
+def test_minus(a, b, out, calculator):
+    assert calculator.minus(a, b) == out
