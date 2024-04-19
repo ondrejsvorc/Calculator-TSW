49f386e2cd0b55f1dcd164ebb70380f8d0f9ba8a
Ondřej Švorc <ondrejsvorc@email.cz>
2024-04-19 18:45:21 +0200

    Update cal_scitani.py

--- cal_scitani.py
+++ cal_scitani.py
@@ -1,20 +1,20 @@
 from operations import *
 
+
 class Calculator:
     def __init__(self):
         self._ans = 0
 
     @property
     def ans(self) -> int:
-        return self._ans 
+        return self._ans
 
     def _operate(self, a, b, operator):
         self._ans = operator(a, b)
         return self._ans
-        
 
     def plus(self, a, b):
         return self._operate(a, b, operator=add)
 
     def minus(self, a, b):
-        return self._operate(a, b, operator=sub)
+        return self._operate(a, b, operator=add)
