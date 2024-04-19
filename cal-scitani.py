a99d58d374b580aa3fcb4a6ea70927510a79aeba
Danil5115 <kordp51@gmail.com>
2024-04-19 17:14:41 +0200

    + bylo udelano

---
+++ cal-scitani.py
@@ -0,0 +1,17 @@
+from operations import *
+
+class Calculator:
+    def __init__(self):
+        self._ans = 0
+
+    @property
+    def ans(self) -> int:
+        return self._ans 
+
+    def _operate(self, a, b, operator):
+        self._ans = operator(a, b)
+        return self._ans
+        
+
+    def plus(self, a, b):
+        return self._operate(a, b, operator=add)
